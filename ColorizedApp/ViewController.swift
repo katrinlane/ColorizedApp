951886c3fb4de8493108b825903335e7050ff2c6
katrinlane <90476944+katrinlane@users.noreply.github.com>
2022-10-29 23:42:12 +0300

    1. Add constraints for elements 2. Colorise view

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - IB Outlets
+    @IBOutlet var colorizedView: UIView!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var redComponentNumber: UILabel!
+    @IBOutlet var greenComponentNumber: UILabel!
+    @IBOutlet var blueComponentNumber: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorizedView.layer.cornerRadius = 20
+        setupSlidersColor()
+        //colorizedView.layer.backgroundColor = .init(red: 1, green: 0.7, blue: 0, alpha: 0.7)
     }
-
-
+    
+    // MARK: - IB Actions
+    @IBAction func redSliderAction() {
+        redComponentNumber.text = String(format: "%.2f", redSlider.value)
+        colorizedView.layer.backgroundColor = .init(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    @IBAction func greenSliderAction() {
+        greenComponentNumber.text = String(format: "%.2f", greenSlider.value)
+        colorizedView.layer.backgroundColor = .init(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    @IBAction func blueSliderAction() {
+        blueComponentNumber.text = String(format: "%.2f", blueSlider.value)
+        colorizedView.layer.backgroundColor = .init(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    // MARK: - Private Methods
+    private func setupSlidersColor() {
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+    }
+    
 }
 
